cc899f8d33255cac636d64d487df1a17d9589361
khnadvemukesh <khandvemukesh@gmail.com>
2023-09-07 18:11:04 +0530

    Rounting Example

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/news', function () {
-//     return view('news');
-// });
+Route::get('/news/{id?}/comment/{commentId}', function (string $id=null, string $commentId=null) {
+     if($id){
+           return '<h1>News Id = '.$id.'  '.$commentId.'<h1> ';}
+           else{
+            return'<h1>No Id Found!<h1>';
+            }
+})->whereAlphaNumeric('id');
 
-Route::view('/news', 'news');
+// Route::view('/news', 'news');
+
+Route::fallback(function(){
+  return '<h2> Page Not Found!</h2>';
+});
